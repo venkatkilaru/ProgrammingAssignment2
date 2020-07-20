c5aeb2a911a6d7a8edebee447ea857dae420c239
venkatkilaru <venkatkilaru117@gmail.com>
2020-07-20 10:13:41 -0700

    for creating and finding inverse have been added

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv<- NULL
+  set<- function(y){
+    x<<-y
+    inv<- NULL
+  }
+  get<- function(){x}
+  
+  
+  getInverse<- function(){inv}
+  setInverse<- function(inverse){
+    inv<<- inverse
+  }
+  
+  list(get=get,set=set,getInverse=getInverse,setInverse=setInverse)
+  
 
 }
 
@@ -12,4 +27,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inv<- x$getInverse()
+  if(!is.null(inv))
+  {
+    message("inverse already exists")
+    return(inv)
+  }
+  
+  mat<- x$get()
+  inv<- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
